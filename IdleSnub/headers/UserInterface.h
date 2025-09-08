34ab7a0d35c16f8ea7adad3ef36202bea0bcab8a
Fatlum Hasaj <fatlum.hasaj@outlook.com>
2025-09-08 23:54:08 +0200

    Updated UI to allow tabs

--- IdleSnub/headers/UserInterface.h
+++ IdleSnub/headers/UserInterface.h
@@ -3,6 +3,8 @@
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/component/component.hpp>
+#include <ftxui/component/component_options.hpp>
+#include <ftxui/component/component_base.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <iostream>
 
@@ -27,6 +29,20 @@ private:
 	int _screenWidth{ 0 };		//Screen Width
 	int _screenHieght{ 0 };		//Screen Hight
 	bool is_running{ true };	// Toggle state
+
+
+
+	//Vectorfor horizonta Tabs
+
+	std::vector<std::string> _tabNames{"Main", "Setting", "About"};
+	std::vector<std::string> _tab1Values{ "Forest", "Water", "I don't know", };
+
+
+
+	int _tabSelected	{ 0 }; 
+	int _tabSelteced1	{ 0 };
+	int _tabSelteced2	{ 0 };
+	int _tabSelteced3	{ 0 };
 };
 
 
